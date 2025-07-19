e7e1927d6d029142649f5a457e6fadb6e7630631
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-19 01:53:43 +0100

    update all content

---
+++ lip.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navmenu =document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () =>{
+    navMenu.classList.toggle("active");
+});
